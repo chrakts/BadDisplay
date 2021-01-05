ac8a85adfe0b4cd6016d9835ec1c000883d57d04
Christof <chrak@cmja.de>
2021-01-05 21:58:28 +0100

    Volatile hinzu

--- Globals.cpp
+++ Globals.cpp
@@ -20,16 +20,17 @@ uint8_t  u8StatusLuefterSoll   = FAN_STATUS_1;
 uint8_t  u8StatusLuefterIst    = FAN_STATUS_1;
 uint8_t  u8StatusHeizungSoll   = HEAT_AUTO;
 uint8_t  u8StatusHeizungIst    = HEAT_AUTO;
-double   fThreshLuefter1       = 50.0;
-double   fThreshLuefter2       = 50.0;
-double   fHystLuefter1         = 5.0;
-double   fHystLuefter2         = 5.0;
-double   fThreshTemp           = 22.0;
-double   fThreshHyst           = 5.0;
-double   fTemperaturIst        = 23.44896;
-double   fFeuchteIst           = 60.568;
-double   fMqttTime             = 1111111111;
-uint32_t u32SecondsCounter     = 1545264000+3600+900;
+volatile double   fThreshLuefter1       = 50.0;
+volatile double   fThreshLuefter2       = 50.0;
+volatile double   fHystLuefter1         = 5.0;
+volatile double   fHystLuefter2         = 5.0;
+volatile double   fThreshTemp           = 22.0;
+volatile double   fThreshHyst           = 5.0;
+volatile double   fTemperaturIst        = 23.44896;
+volatile double   fFeuchteIst           = 60.568;
+volatile double   fMqttTime             = 1111111111;
+volatile uint32_t u32SecondsCounter     = 1545264000+3600+900;
+
 
 uint8_t displayStatus = DISPLAY_MAIN;
 uint8_t oldDisplayStatus = DISPLAY_NONSENSE;
