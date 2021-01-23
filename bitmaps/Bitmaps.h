e5c80bfe36275b318e84205434fb7f90a07f09c7
Christof <chrak@cmja.de>
2021-01-23 21:05:52 +0100

    Umbenennung

---
+++ bitmaps/Bitmaps.h
@@ -0,0 +1,30 @@
+#ifndef BITMAPS_H_
+#define BITMAPS_H_
+
+#include <avr/pgmspace.h>
+
+extern const char data_LuefterAus_cbmp[] PROGMEM;
+extern const char data_SetT1H_cbmp[] PROGMEM;
+extern const char data_SetL1_cbmp[] PROGMEM;
+extern const char data_fanSpeed_1_cbmp[] PROGMEM;
+extern const char data_SetL2H_cbmp[] PROGMEM;
+extern const char data_fanOff_cbmp[] PROGMEM;
+extern const char data_plusOutline_cbmp[] PROGMEM;
+extern const char data_fanAuto_cbmp[] PROGMEM;
+extern const char data_HeizungAn_cbmp[] PROGMEM;
+extern const char data_SetL2_cbmp[] PROGMEM;
+extern const char data_Luefter1_cbmp[] PROGMEM;
+extern const char data_minusOutline_cbmp[] PROGMEM;
+extern const char data_Plus_cbmp[] PROGMEM;
+extern const char data_HeizungAus_cbmp[] PROGMEM;
+extern const char data_fanSpeed_2_cbmp[] PROGMEM;
+extern const char data_SetL1H_cbmp[] PROGMEM;
+extern const char data_closeOutline_cbmp[] PROGMEM;
+extern const char data_Set_cbmp[] PROGMEM;
+extern const char data_SetT1_cbmp[] PROGMEM;
+extern const char data_Luefter2_cbmp[] PROGMEM;
+extern const char data_Minus_cbmp[] PROGMEM;
+extern const char data_checkOutline_cbmp[] PROGMEM;
+extern const char data_LuefterAuto_cbmp[] PROGMEM;
+
+#endif /* BITMAPS_H_ */
