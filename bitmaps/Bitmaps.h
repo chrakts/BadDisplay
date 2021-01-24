fe26163b2fb96ec6ac636d121f93ecbe55e3b2dd
Christof <chrak@cmja.de>
2021-01-24 16:17:44 +0100

    neue Bilder

--- bitmaps/Bitmaps.h
+++ bitmaps/Bitmaps.h
@@ -3,28 +3,34 @@
 
 #include <avr/pgmspace.h>
 
-extern const char data_LuefterAus_cbmp[] PROGMEM;
-extern const char data_SetT1H_cbmp[] PROGMEM;
-extern const char data_SetL1_cbmp[] PROGMEM;
-extern const char data_fanSpeed_1_cbmp[] PROGMEM;
-extern const char data_SetL2H_cbmp[] PROGMEM;
-extern const char data_fanOff_cbmp[] PROGMEM;
-extern const char data_plusOutline_cbmp[] PROGMEM;
-extern const char data_fanAuto_cbmp[] PROGMEM;
 extern const char data_HeizungAn_cbmp[] PROGMEM;
-extern const char data_SetL2_cbmp[] PROGMEM;
-extern const char data_Luefter1_cbmp[] PROGMEM;
-extern const char data_minusOutline_cbmp[] PROGMEM;
-extern const char data_Plus_cbmp[] PROGMEM;
 extern const char data_HeizungAus_cbmp[] PROGMEM;
-extern const char data_fanSpeed_2_cbmp[] PROGMEM;
-extern const char data_SetL1H_cbmp[] PROGMEM;
-extern const char data_closeOutline_cbmp[] PROGMEM;
-extern const char data_Set_cbmp[] PROGMEM;
-extern const char data_SetT1_cbmp[] PROGMEM;
+extern const char data_Luefter1_cbmp[] PROGMEM;
 extern const char data_Luefter2_cbmp[] PROGMEM;
-extern const char data_Minus_cbmp[] PROGMEM;
-extern const char data_checkOutline_cbmp[] PROGMEM;
+extern const char data_LuefterAus_cbmp[] PROGMEM;
 extern const char data_LuefterAuto_cbmp[] PROGMEM;
+extern const char data_Minus_cbmp[] PROGMEM;
+extern const char data_Plus_cbmp[] PROGMEM;
+extern const char data_Set_cbmp[] PROGMEM;
+extern const char data_SetL1_cbmp[] PROGMEM;
+extern const char data_SetL1H_cbmp[] PROGMEM;
+extern const char data_SetL2_cbmp[] PROGMEM;
+extern const char data_SetL2H_cbmp[] PROGMEM;
+extern const char data_SetT1_cbmp[] PROGMEM;
+extern const char data_SetT1H_cbmp[] PROGMEM;
+extern const char data_checkOutline_cbmp[] PROGMEM;
+extern const char data_closeOutline_cbmp[] PROGMEM;
+extern const char data_fanAuto_cbmp[] PROGMEM;
+extern const char data_fanOff_cbmp[] PROGMEM;
+extern const char data_fanSpeed_1_cbmp[] PROGMEM;
+extern const char data_fanSpeed_2_cbmp[] PROGMEM;
+extern const char data_minusOutline_cbmp[] PROGMEM;
+extern const char data_plusOutline_cbmp[] PROGMEM;
+extern const char data_radiator_cbmp[] PROGMEM;
+extern const char data_radiatorAuto_cbmp[] PROGMEM;
+extern const char data_radiatorDay_cbmp[] PROGMEM;
+extern const char data_radiatorHysterese_cbmp[] PROGMEM;
+extern const char data_radiatorNight_cbmp[] PROGMEM;
+extern const char data_radiatorOff_cbmp[] PROGMEM;
 
 #endif /* BITMAPS_H_ */
